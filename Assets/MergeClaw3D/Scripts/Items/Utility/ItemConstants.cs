fe1a89dec362bbc21310ff4e4c85353df2aab3d2
madeflyoke <sano555000@gmail.com>
2024-07-22 12:10:33 +0300

    upd: items size increase

--- Assets/MergeClaw3D/Scripts/Items/Utility/ItemConstants.cs
+++ Assets/MergeClaw3D/Scripts/Items/Utility/ItemConstants.cs
@@ -4,9 +4,9 @@
     {
         public const int ITEMS_GROUP_COUNT = 3;
         
-        public const float LARGE_ITEM_SCALE = 2f;
-        public const float MEDIUM_ITEM_SCALE = 1.7f;
-        public const float SMALL_ITEM_SCALE = 1.4f;
+        public const float LARGE_ITEM_SCALE = 2.2f;
+        public const float MEDIUM_ITEM_SCALE = 2f;
+        public const float SMALL_ITEM_SCALE = 1.8f;
 
         public const int DEFAULT_ITEM_LAYER = 7;
         public const int IGNORED_ITEM_LAYER = 8;
